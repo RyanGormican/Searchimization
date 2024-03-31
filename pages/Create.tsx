c8feff946f7f898f11ececd5fb1b34ce07d9d20b
RyanGormican <ryan.gormican@gmail.com>
2024-03-30 18:04:07 -0600

    a
    a

--- pages/Create.tsx
+++ pages/Create.tsx
@@ -1,12 +1,60 @@
 import React from 'react';
 import '/src/app/globals.css';
+import Link from 'next/link';
+import { Icon } from '@iconify/react';
+import { useState, useRef ,useEffect} from "react";
 const Create = () => {
-  return (
-    <div>
-      <h1>Create Page</h1>
-    
+ const gridRef = useRef(null);
+ const [gridContent, setGridContent] = useState([]);
+ const [grouping, setGrouping] = useState(1);
 
-    </div>
+  return (
+   <main className="flex min-h-screen items-center p-12 flex-col">
+      <div className="text-3xl font-bold mb-4">Strandimization</div>
+         <div className="links">
+          <a href="https://www.linkedin.com/in/ryangormican/">
+            <Icon icon="mdi:linkedin" color="#0e76a8" width="60" />
+          </a>
+          <a href="https://github.com/RyanGormican/Strandimization">
+            <Icon icon="mdi:github" color="#e8eaea" width="60" />
+          </a>
+          <a href="https://ryangormicanportfoliohub.vercel.app/">
+            <Icon icon="teenyicons:computer-outline" color="#199c35" width="60" />
+          </a>
+        </div>
+<div className="flex">
+<div>
+          <Link href="/Puzzles">
+            <button className="py-2 px-4 bg-blue-500 text-white rounded">PUZZLES</button>
+          </Link>
+        </div>
+        <div>
+          <Link href="/Create">
+            <button className="py-2 px-4 bg-blue-500 text-white rounded">CREATE</button>
+          </Link>
+        </div>
+</div>
+        <div
+  ref={gridRef}
+  className="grid grid-cols-6 grid-rows-8 gap-4"
+>
+  {gridContent &&
+    gridContent.map(({ letter, group, position, index }) => (
+      <div
+        key={index}
+        onClick={() => handleClick(index)}
+      >
+        <div
+          
+        >
+          {letter}
+        </div>
+      </div>
+    ))}
+</div>
+
+  
+    </main>
   );
 }
 
