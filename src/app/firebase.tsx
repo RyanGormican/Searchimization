5855320558ee5547a570f48a931149e3b2adc2ba
RyanGormican <ryan.gormican@gmail.com>
2024-04-01 00:13:25 -0600

    a
    a

--- src/app/firebase.tsx
+++ src/app/firebase.tsx
@@ -11,6 +11,7 @@ const firebaseConfig = {
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
   measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
+
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const firestore = getFirestore(app); 
