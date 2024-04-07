7bd34f88e20dd12b6b401d173f5894fdc20412a7
RyanGormican <ryan.gormican@gmail.com>
2024-04-07 05:07:39 -0600

    a
    a

--- pages/_document.js
+++ pages/_document.js
@@ -8,7 +8,6 @@ class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
           <link href="https://fonts.googleapis.com/css2?family=Bubblegum+Sans&family=Doppio+One&family=IBM+Plex+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&family=Jost:ital,wght@1,300&family=Lato&family=Oswald:wght@500&family=Sora:wght@100..800&family=Truculenta:opsz,wght@12..72,100..900&display=swap" rel="stylesheet" />
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link rel="icon" href="/favicon.ico" />
         </Head>
         <body>
