3f3edbe93cc8fce7af04bbe1d21761f7b9a6c39d
RyanGormican <ryan.gormican@gmail.com>
2024-04-08 22:23:56 -0600

    a
    a

--- pages/Play/[id].tsx
+++ pages/Play/[id].tsx
@@ -1,13 +1,11 @@
-'use client'
-import '/src/app/globals.css';
 import { useState, useRef, useEffect } from "react";
-import { Icon } from '@iconify/react';
-import Link from 'next/link';
 import { useRouter } from 'next/router';
-import { collection, doc, getDoc, updateDoc, increment, DocumentData } from 'firebase/firestore';
+import { doc, getDoc, updateDoc, increment } from 'firebase/firestore';
 import { auth, firestore } from '../../src/app/firebase';
 import Header from '../../src/app/Header';
-// Interface for each grid item
+import '/src/app/globals.css';
+
+// Define interfaces for grid items and puzzle data
 interface GridItem {
   letter: string;
   group: number;
@@ -15,117 +13,97 @@ interface GridItem {
   index: number;
   found: boolean;
 }
-// Interface for local storage
-interface SearchimizationEntry {
-  id: string;
-}
-// Interface for puzzle data
+
 interface PuzzleData {
   theme: string;
   gridContent: GridItem[];
 }
 
+// Define the main component
 const Play = () => {
+  // Access router and query parameters
   const router = useRouter();
   const { id } = router.query;
 
-  // State for puzzle theme
+  // State variables for puzzle theme, grid content, found words, selected letters, mouse down state,
+  // found indexes, start time, end time, and fast times
   const [theme, setTheme] = useState<PuzzleData | null>(null);
-
-  // State for grid content
   const [gridContent, setGridContent] = useState<GridItem[]>([]);
-
-  // State for number of found words
   const [foundWords, setFoundWords] = useState<number>(0);
+  const [selectedLetters, setSelectedLetters] = useState<number[]>([]);
+  const [isMouseDown, setIsMouseDown] = useState<boolean>(false);
+  const [foundIndexes, setFoundIndexes] = useState<number[]>([]);
+  const [startTime, setStartTime] = useState<number | null>(null);
+  const [endTime, setEndTime] = useState<number | null>(null);
+  const [fastTimes, setFastTimes] = useState<Array<{ time: number; username: string; userId: string }>>([]);
 
   // Reference for the grid div
   const gridRef = useRef<HTMLDivElement>(null);
 
-  // State for selected letters
-  const [selectedLetters, setSelectedLetters] = useState<number[]>([]);
-
-  // State for mouse down event
-  const [isMouseDown, setIsMouseDown] = useState<boolean>(false);
-
-  // State for found indexes
-  const [foundIndexes, setFoundIndexes] = useState<number[]>([]);
-
-  useEffect(() => {
-
-// Check local storage for puzzle data before fetching from Firestore
-// Check local storage for puzzle data before fetching from Firestore
-const fetchPuzzleData = async () => {
-  try {
-    if (!id) return;
-
-    // Check if puzzle data exists in local storage
-const searchimizationData = JSON.parse(localStorage.getItem('searchimization') || '{}');
-
-    if (searchimizationData && searchimizationData.entries) {
-const puzzleFromStorage = searchimizationData.entries.find((entry: SearchimizationEntry) => entry.id === id);
-      if (puzzleFromStorage) {
-        setTheme(puzzleFromStorage);
-        setGridContent(puzzleFromStorage.gridContent); 
-            const updatedProfile = {
-              ...searchimizationData.profile,
-              sessionplays: (searchimizationData.profile.sessionplays || 0) + 1
-            };
-            localStorage.setItem('searchimization', JSON.stringify({ ...searchimizationData, profile: updatedProfile }));
-
-        // Increment plays count and update last updated timestamp
-        const puzzleDocRef = doc(firestore, 'puzzles', id as string);
-        await updateDoc(puzzleDocRef, {
-          plays: increment(1),
-          lastupdated: new Date().toISOString()
-        });
-        return;
-      }
-    }
-
-    // Fetch puzzle data from Firestore if not found in local storage
-    const puzzleDocRef = doc(firestore, 'puzzles', id as string);
-    const puzzleDoc = await getDoc(puzzleDocRef);
-    const puzzleData = puzzleDoc.data() as PuzzleData;
-    setTheme(puzzleData);
-    setGridContent(puzzleData.gridContent); // Set grid content state
-
-    // Save puzzle data to local storage
-    const updatedEntries: SearchimizationEntry[] = searchimizationData.entries.map((entry: SearchimizationEntry) => entry.id === id ? puzzleData : entry);
-    localStorage.setItem('searchimization', JSON.stringify({ ...searchimizationData, entries: updatedEntries }));
-
-    // Increment plays count and update last updated timestamp
-    await updateDoc(puzzleDocRef, {
-      plays: increment(1),
-      lastupdated: new Date().toISOString()
-    });
-  } catch (error) {
-    console.error('Error fetching puzzle data:', error);
-  }
-};
-
-
-
-    fetchPuzzleData();
-
-  }, [id]);
-
-  // Reset selected letters
-  const resetSelect = () => {
-    setSelectedLetters([]);
+  // Function to start the timer
+  const startTimer = () => {
+    setStartTime(Date.now());
   };
 
-  // Mouse up event handler
+  // Function to stop the timer and update fast times
+  const stopTimer = async () => {
+    const currentTime = Date.now();
+    if (startTime && currentTime > startTime) {
+      const elapsedTime = currentTime - startTime;
+      setEndTime(currentTime);
+      if (auth.currentUser) {
+        const user = auth.currentUser;
+        const timeData = { time: elapsedTime, username: user.displayName ?? user.email, userId: user.uid };
+
+        // Update fast times in Firestore
+        const puzzleRef = doc(firestore, 'puzzles', id as string);
+        const puzzleDoc = await getDoc(puzzleRef);
+        const puzzleData = puzzleDoc.data();
+        const currentFastTimes = puzzleData?.fastTimes || [];
+
+        const updatedFastTimes = [...currentFastTimes, timeData].sort((a, b) => a.time - b.time).slice(0, 10);
+
+        await updateDoc(puzzleRef, { fastTimes: updatedFastTimes });
+        setFastTimes(updatedFastTimes);
+      }
+    } else {
+      console.error("Invalid timer start or end time.");
+    }
+  };
+
+  // Function to fetch puzzle data from Firestore
+  const fetchPuzzleData = async () => {
+    try {
+      if (!id) return;
+
+      const puzzleDocRef = doc(firestore, 'puzzles', id as string);
+      const puzzleDoc = await getDoc(puzzleDocRef);
+      const puzzleData = puzzleDoc.data() as PuzzleData;
+      setTheme(puzzleData);
+      setGridContent(puzzleData.gridContent);
+    } catch (error) {
+      console.error('Error fetching puzzle data:', error);
+    }
+  };
+
+  // Event handler for mouse up
   const handleMouseUp = () => {
     setIsMouseDown(false);
   };
 
-  // Submit word handler
+  // Event handler for clicking grid cells
+  const handleClick = (index: number) => {
+    if (!selectedLetters.includes(index)) {
+      setSelectedLetters([...selectedLetters, index]);
+    }
+  };
+
+  // Function to submit a word
   const submitWord = () => {
     const selectedWordIndexes = [...selectedLetters];
 
-    // Check if selected word is found
+    // Iterate through each grid item to find the selected word
     for (let i = 0; i < gridContent.length; i++) {
-      // Find word group
       const wordGroup: GridItem[] = [];
       let j = i;
       while (j < gridContent.length && gridContent[j].group === gridContent[i].group) {
@@ -133,7 +111,7 @@ const puzzleFromStorage = searchimizationData.entries.find((entry: Searchimizati
         j++;
       }
 
-      // Group indexes
+      // Group indexes for each group
       const groupIndexes: { [key: number]: number[] } = {};
       gridContent.forEach(({ group, index, position }) => {
         if (!groupIndexes[group]) {
@@ -142,7 +120,7 @@ const puzzleFromStorage = searchimizationData.entries.find((entry: Searchimizati
         groupIndexes[group].push(index);
       });
 
-      // Sort group indexes
+      // Sort group indexes by position
       for (const group in groupIndexes) {
         groupIndexes[group].sort((a, b) => {
           const posA = gridContent.find(item => item.index === a)?.position ?? 0;
@@ -151,7 +129,7 @@ const puzzleFromStorage = searchimizationData.entries.find((entry: Searchimizati
         });
       }
 
-      // Check if selected word is found in any group
+      // Check if the selected word matches any group
       for (const group in groupIndexes) {
         const groupIndex = groupIndexes[group];
 
@@ -172,48 +150,36 @@ const puzzleFromStorage = searchimizationData.entries.find((entry: Searchimizati
     setSelectedLetters([]);
   };
 
-  // Click event handler for grid cells
-  const handleClick = (index: number) => {
-    if (!selectedLetters.includes(index)) {
-      setSelectedLetters([...selectedLetters, index]);
-    }
+  // Function to reset selected letters
+  const resetSelect = () => {
+    setSelectedLetters([]);
   };
 
   // Calculate maximum group number
   const maxGroup = Math.max(...gridContent.map(item => item.group));
 
+  // Effect to fetch puzzle data and start the timer on component mount
   useEffect(() => {
-    // Increment finishes count if all words are found
+    fetchPuzzleData();
+    startTimer();
+  }, [id]);
+
+  // Effect to stop the timer when all words are found
+  useEffect(() => {
     if (foundWords > 0 && foundWords === maxGroup) {
-      const incrementFinishes = async () => {
-        try {
-          const puzzleRef = doc(firestore, 'puzzles', id as string);
-          await updateDoc(puzzleRef, {
-            finishes: increment(1),
-            lastupdated: new Date().toISOString()
-          });
-             const searchimizationData = JSON.parse(localStorage.getItem('searchimization') || '{}');
-          const updatedProfile = {
-            ...searchimizationData.profile,
-            sessionfinishes: (searchimizationData.profile.sessionfinishes || 0) + 1
-          };
-          localStorage.setItem('searchimization', JSON.stringify({ ...searchimizationData, profile: updatedProfile }));
-        } catch (error) {
-          console.error('Error incrementing finishes:', error);
-        }
-      };
-      incrementFinishes();
+      stopTimer();
     }
-  }, [foundWords, maxGroup, id]);
+  }, [foundWords]);
+
 
   return (
     <main className="flex min-h-screen items-center p-12 flex-col">
-          <Header currentUser={auth.currentUser} />
+      <Header currentUser={auth.currentUser} />
       <div className="flex flex-col mb-4">
         <h1 className="font-bold">Your Theme</h1>
         <p className="mb-4">{theme && theme.theme}</p>
         <h2>
-        <p>{foundWords} of {maxGroup} theme words found</p>
+          <p>{foundWords} of {maxGroup} theme words found</p>
         </h2>
         <div style={{ minHeight: "50px", maxHeight: "50px" }}>
           {selectedLetters.length > 0 && (
