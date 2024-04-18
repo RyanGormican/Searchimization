003475afa23f0976aca31cdc6e195b08e2882c08
RyanGormican <ryan.gormican@gmail.com>
2024-04-18 04:06:36 -0600

    a
    a

--- pages/Create.tsx
+++ pages/Create.tsx
@@ -71,7 +71,6 @@ const newGridContent = Array.from({ length: 48 }, (_, index) => ({
 const newGridContent = Array.from({ length: 100 }, (_, index) => ({
       letter: 'A',
       group: 1,
-      position:1
       index,
       found: false
     }));
